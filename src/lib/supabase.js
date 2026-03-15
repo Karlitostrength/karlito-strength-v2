cd440026659f6dbf7a94a87bfd16a1df111c3a9e
Karlitostrength <karolprzybycien91@gmail.com>
2026-03-15 11:43:45 +0000

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://ebpdfalmzkvxfuzaamqh.supabase.co",
+  "sb_publishable_-9TPMx_XdGI0Ur5m-Utqeg_drghUgIy"
+);
